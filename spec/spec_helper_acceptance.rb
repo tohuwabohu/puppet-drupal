fca173c889a4dd9b9cd7eaef12c3c011b74ab9b3
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-11-01 20:29:37 +1100

    Ignore files during the acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,13 +12,14 @@ end
 
 RSpec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  ignore_list = %w(junit log spec tests vendor)
 
   c.formatter = :documentation
 
   c.before :suite do
     hosts.each do |host|
       # Install module
-      copy_module_to(host, :source => proj_root, :module_name => 'drupal')
+      copy_module_to(host, :source => proj_root, :module_name => 'drupal', :ignore_list => ignore_list)
 
       # Install dependencies
       on host, puppet('module', 'install', 'puppetlabs-vcsrepo', '--version 1.1.0')
