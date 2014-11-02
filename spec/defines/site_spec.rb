96f5c02fcdc4e90d367ae8d9a2467324e022c410
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-11-02 16:46:48 +1100

    Consistence

--- spec/defines/site_spec.rb
+++ spec/defines/site_spec.rb
@@ -9,7 +9,7 @@ describe 'drupal::site' do
 
     specify { should contain_file(make_file).with_ensure('file') }
     specify { should contain_file(make_file).with_content(/core = 6.x/) }
-    specify { should contain_file(make_file).with_content(/projects\[drupal\] = 6\.33/) }
+    specify { should contain_file(make_file).with_content(/projects\[drupal\]\[version\] = 6\.33/) }
   end
 
   describe 'with core_version => 7.32' do
@@ -17,7 +17,7 @@ describe 'drupal::site' do
 
     specify { should contain_file(make_file).with_ensure('file') }
     specify { should contain_file(make_file).with_content(/core = 7.x/) }
-    specify { should contain_file(make_file).with_content(/projects\[drupal\] = 7\.32/) }
+    specify { should contain_file(make_file).with_content(/projects\[drupal\]\[version\] = 7\.32/) }
   end
 
   describe 'with modules view' do
