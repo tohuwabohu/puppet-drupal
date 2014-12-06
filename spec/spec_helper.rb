80269bbe8ea9c6d0c5757d8c2911e1c1257d9dea
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-12-06 15:01:10 +1100

    Better imports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'fixtures/modules/module_data/lib/hiera/backend/module_data_backend.rb'
+require 'fixtures/modules/module_data/lib/hiera/backend/module_data_backend'
 
 RSpec.configure do |c|
   c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
