4f60af33b995c1815a04a0631006791c73302721
Martin Meinhold <Martin.Meinhold@gmx.de>
2015-05-17 18:44:56 +1000

    Add URI to cron command (update script)
    This will ensure the generated emails will contain the

--- manifests/site.pp
+++ manifests/site.pp
@@ -146,7 +146,7 @@ define drupal::site (
   $real_process = pick($process, $drupal::www_process)
 
   $drush_build_site = "${drupal::drush_path} make --verbose --concurrency=${drupal::drush_concurrency_level} ${config_file} ${drupal_site_dir} >> ${drupal::log_dir}/${title}.log 2>&1"
-  $drush_update_database = "${drupal::update_script_path} ${drupal_site_dir} >> ${drupal::log_dir}/${title}.log 2>&1"
+  $drush_update_database = "${drupal::update_script_path} ${drupal_site_dir} ${title} >> ${drupal::log_dir}/${title}.log 2>&1"
   $drush_check_pending_database_updates = "${drupal::drush_path} updatedb-status --pipe --root=${drupal_site_dir} 2>&1"
 
   $real_database_updates_disable = $database_updates_disable ? {
