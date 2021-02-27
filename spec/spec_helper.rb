89fa633f25ae785c642b890df7871e3129a43d95
Martin Meinhold <Martin.Meinhold@gmx.de>
2021-02-27 13:12:21 +0100

    Update test infrastructure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.default_facts = {
-    :root_home => '/root',
-    :path      => '/bin:/usr/bin',
+    # archive
+    :path            => '/usr/bin',
+
+    :root_home       => '/root',
   }
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,19 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
+# collection: puppet - latest, puppet6 is the current version
+install_puppet_agent_on(hosts, {:puppet_collection => 'puppet' } )
 install_module_on(hosts)
 install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 4.15.0')
 install_module_from_forge_on(hosts, 'camptocamp-archive', '= 0.9.0')
+
+RSpec.configure do |c|
+  c.formatter = :documentation
+
+  c.before :suite do
+    logger.info("Using Puppet version #{(on default, 'puppet --version').stdout.chomp}")
+  end
+end
+
