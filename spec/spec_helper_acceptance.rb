b569680a971bb21e71a002769ec368b12ea3887a
Martin Meinhold <Martin.Meinhold@gmx.de>
2024-02-18 15:42:43 +0100

    Update Puppet dependencies

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,8 +6,8 @@ require 'beaker/module_install_helper'
 # collection: puppet - latest, puppet6 is the current version
 install_puppet_agent_on(hosts, {:puppet_collection => 'puppet' } )
 install_module_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 9.1.0')
-install_module_from_forge_on(hosts, 'puppet-archive', '= 7.0.0')
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 9.4.1')
+install_module_from_forge_on(hosts, 'puppet-archive', '= 7.1.0')
 
 RSpec.configure do |c|
   c.formatter = :documentation
